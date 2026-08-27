9ef5ad5430deb56885542fb9b6eb1e23ca0e1c6c
Ruwani Ranthika <ruwanielv@gmail.com>
2026-08-27 23:22:43 +0530

    fix: add explicit ComponentScan for package scanning

--- src/main/java/com/eca/backend/userservice/UserServiceApplication.java
+++ src/main/java/com/eca/backend/userservice/UserServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan(basePackages = "com.eca.backend.userservice")
 @EnableJpaRepositories(basePackages = "com.eca.backend.userservice.repository")
 @EntityScan(basePackages = "com.eca.backend.userservice.model")
 public class UserServiceApplication {
