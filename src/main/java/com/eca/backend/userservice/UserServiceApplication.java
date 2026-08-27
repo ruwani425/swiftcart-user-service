697df79b090122b3947e2ba89f7cc6b742389fd9
Ruwani Ranthika <ruwanielv@gmail.com>
2026-08-27 22:16:48 +0530

    fix: add explicit ComponentScan

--- src/main/java/com/eca/backend/userservice/UserServiceApplication.java
+++ src/main/java/com/eca/backend/userservice/UserServiceApplication.java
@@ -3,9 +3,11 @@ package com.eca.backend.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan(basePackages = "com.eca.backend.userservice")
 public class UserServiceApplication {
 
     public static void main(String[] args) {
