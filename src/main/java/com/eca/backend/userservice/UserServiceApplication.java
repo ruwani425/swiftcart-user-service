b6df75cb420ed958f92f70e420898359e5fb9212
Ruwani Ranthika <ruwanielv@gmail.com>
2026-08-27 22:55:50 +0530

    fix: add explicit EnableJpaRepositories and EntityScan

--- src/main/java/com/eca/backend/userservice/UserServiceApplication.java
+++ src/main/java/com/eca/backend/userservice/UserServiceApplication.java
@@ -2,10 +2,14 @@ package com.eca.backend.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableJpaRepositories(basePackages = "com.eca.backend.userservice.repository")
+@EntityScan(basePackages = "com.eca.backend.userservice.model")
 public class UserServiceApplication {
 
     public static void main(String[] args) {
